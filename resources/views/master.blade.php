b7708e8c6a2bb2366ede7e7c64b8b332d7dcde88
Minh Cat <taminhcatcn12@gmail.com>
2019-05-12 10:21:58 +0700

    add header

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,9 +4,37 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- bootstrap -->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap-theme.css">
+	<script type="text/javascript" src="vendor/jquery/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="vendor/bootstrap/js/bootstrap.js"></script>
+	<!-- end bootstrap -->
+
+	<!-- font awesome -->
+	<link rel="stylesheet" type="text/css" href="vendor/font-awesome/css/font-awesome.css">
+	<!-- end font awesome -->
+
+	<!-- slick -->
+	<script type="text/javascript" src="vendor/slick/slick.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="vendor/slick/slick.css">
+	<link rel="stylesheet" type="text/css" href="vendor/slick/slick-theme.css">
+	<!-- end slick -->
+
 	<title>My Book</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/header.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+	<link rel="stylesheet" type="text/css" href="css/sidebar.css">
+	<link rel="icon" href="image/icon.png">
+	<script type="text/javascript" src="jquery/index.js"></script>
+	<script type="text/javascript" src="jquery/common.js"></script>
 </head>
 <body>
+	@include('pages.content_header')
+
 	@yield('content')
 </body>
 </html>
