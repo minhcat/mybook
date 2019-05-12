5f828c4757d03bc923de5ccc20ebf1b112d5f23e
Minh Cat <taminhcatcn12@gmail.com>
2019-05-12 08:41:58 +0700

    test project

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,7 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', 'HomeController@index');
 
 Route::get('home', 'HomeController@index');
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
