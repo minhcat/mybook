f9642b972f522ee60893be97faadf3131edb29a5
Minh Cat <taminhcatcn12@gmail.com>
2019-05-18 12:19:28 +0700

    blade small box uploader

---
+++ resources/views/partials/admin/content/small/statistic-contant.blade.php
@@ -0,0 +1,57 @@
+<div class="row small statistic">
+	<div class="col-mb-5">
+		<div class="small-box bg-blue collapse box-link" data-target="#box-book-chart-view" data-small="statistic" data-with="#box-book-list-small-3">
+			<div class="small-box-header">Thống kê</div>
+			<div class="inner">
+				<h4>Lượt Xem</h4>
+			</div>
+			<div class="icon">
+				<i class="fa fa-eye"></i>
+			</div>
+		</div>
+	</div>
+	<div class="col-mb-5">
+		<div class="small-box bg-green collapse box-link" data-target="#box-book-chart-cmd" data-small="statistic" data-with="#box-book-list-small-3">
+			<div class="small-box-header">Thống kê</div>
+			<div class="inner">
+				<h4>Bình Luận</h4>
+			</div>
+			<div class="icon">
+				<i class="fa fa-comment"></i>
+			</div>
+		</div>
+	</div>
+	<div class="col-mb-5">
+		<div class="small-box bg-red collapse box-link" data-target="#box-book-chart-like" data-small="statistic" data-with="#box-book-list-small-3">
+			<div class="small-box-header">Thống kê</div>
+			<div class="inner">
+				<h4>Yêu Thích</h4>
+			</div>
+			<div class="icon">
+				<i class="fa fa-heart"></i>
+			</div>
+		</div>
+	</div>
+	<div class="col-mb-5">
+		<div class="small-box bg-orange collapse box-link" data-target="#box-book-chart-follow" data-small="statistic" data-with="#box-book-list-small-3">
+			<div class="small-box-header">Thống kê</div>
+			<div class="inner">
+				<h4>Theo Dõi</h4>
+			</div>
+			<div class="icon">
+				<i class="fa fa-user"></i>
+			</div>
+		</div>
+	</div>
+	<div class="col-mb-5">
+		<div class="small-box bg-purple collapse box-link" data-target="#box-book-chart-rate" data-small="statistic" data-with="#box-book-list-small-4">
+			<div class="small-box-header">Thống kê</div>
+			<div class="inner">
+				<h4>Đánh Giá</h4>
+			</div>
+			<div class="icon">
+				<i class="fa fa-star"></i>
+			</div>
+		</div>
+	</div>
+</div>
--- resources/views/partials/admin/content/uploader.blade.php
+++ resources/views/partials/admin/content/uploader.blade.php
@@ -15,64 +15,8 @@
 
 			<p>Add the sidebar-collapse class to the body tag to get this layout. You should combine this option with a fixed layout if you have a long sidebar. Doing that will prevent your page content from getting stretched vertically.</p>
 		</div>
-		<!-- Statistac control box -->
-		<div class="row small statistic">
-			<div class="col-mb-5">
-				<div class="small-box bg-blue collapse box-link" data-target="#box-book-chart-view" data-small="statistic" data-with="#box-book-list-small-3">
-					<div class="small-box-header">Thống kê</div>
-					<div class="inner">
-						<h4>Lượt Xem</h4>
-					</div>
-					<div class="icon">
-						<i class="fa fa-eye"></i>
-					</div>
-				</div>
-			</div>
-			<div class="col-mb-5">
-				<div class="small-box bg-green collapse box-link" data-target="#box-book-chart-cmd" data-small="statistic" data-with="#box-book-list-small-3">
-					<div class="small-box-header">Thống kê</div>
-					<div class="inner">
-						<h4>Bình Luận</h4>
-					</div>
-					<div class="icon">
-						<i class="fa fa-comment"></i>
-					</div>
-				</div>
-			</div>
-			<div class="col-mb-5">
-				<div class="small-box bg-red collapse box-link" data-target="#box-book-chart-like" data-small="statistic" data-with="#box-book-list-small-3">
-					<div class="small-box-header">Thống kê</div>
-					<div class="inner">
-						<h4>Yêu Thích</h4>
-					</div>
-					<div class="icon">
-						<i class="fa fa-heart"></i>
-					</div>
-				</div>
-			</div>
-			<div class="col-mb-5">
-				<div class="small-box bg-orange collapse box-link" data-target="#box-book-chart-follow" data-small="statistic" data-with="#box-book-list-small-3">
-					<div class="small-box-header">Thống kê</div>
-					<div class="inner">
-						<h4>Theo Dõi</h4>
-					</div>
-					<div class="icon">
-						<i class="fa fa-user"></i>
-					</div>
-				</div>
-			</div>
-			<div class="col-mb-5">
-				<div class="small-box bg-purple collapse box-link" data-target="#box-book-chart-rate" data-small="statistic" data-with="#box-book-list-small-4">
-					<div class="small-box-header">Thống kê</div>
-					<div class="inner">
-						<h4>Đánh Giá</h4>
-					</div>
-					<div class="icon">
-						<i class="fa fa-star"></i>
-					</div>
-				</div>
-			</div>
-		</div>
+		<!-- Statistic control box -->
+		@include('partials.admin.content.small.statistic-contant')
 
 		<!-- Box -->
 		<div class="row">
