46a7584649488478c827095a01e2b659a4214eec
Minh Cat <taminhcatcn12@gmail.com>
2019-05-18 19:21:01 +0700

    blade admin mod comment

---
+++ resources/views/partials/admin/content/comment/comment-new.blade.php
@@ -0,0 +1,101 @@
+<div class="box box-primary direct-chat direct-chat-primary collapse in" id="box-comment-new" aria-expanded="true">
+	<!-- box header -->
+	<div class="box-header with-border">
+		<h3 class="box-title">Bình Luận Mới</h3>
+
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-remove="collapse"><i class="fa fa-times"></i></button>
+		</div>
+	</div>
+	<!-- /box header -->
+	<!-- box body -->
+	<div class="box-body">
+		<!-- Conversations are loaded here -->
+		<div class="direct-chat-messages">
+			<!-- Message. Default to the left -->
+			<div class="direct-chat-msg">
+				<div class="direct-chat-info clearfix">
+					<span class="direct-chat-name pull-left">Nguyễn Hoài Nam - <span class="direct-chat-timestamp">Overlord chap 10</span> - <span class="direct-chat-timestamp">2:00 pm 23/3/2019</span></span>
+				</div>
+				<!-- /.direct-chat-info -->
+				<img class="direct-chat-img" src="{{ asset('image/admin/user1-128x128.jpg') }}" alt="Message User Image">
+				<!-- /.direct-chat-img -->
+				<div class="direct-chat-text">
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit
+				</div>
+				<!-- tag comment -->
+				<div class="direct-chat-button">
+					<span class="fa fa-plus bg-blue" title="thêm vào danh sách kiểm tra" data-toggle="modal" data-target="#modal-comment-save"></span>
+					<span class="fa fa-flag bg-orange" title="phạt người dùng này" data-toggle="modal" data-target="#modal-comment-punish"></span>
+					<span class="fa fa-ban bg-red" title="cấm đăng nhập người dùng này" data-toggle="modal" data-target="#modal-comment-ban"></span>
+				</div>
+
+				<!-- /.direct-chat-text -->
+			</div>
+			<!-- /.direct-chat-msg -->
+
+			<!-- Message to the right -->
+			<div class="direct-chat-msg">
+				<div class="direct-chat-info clearfix">
+					<span class="direct-chat-name pull-left">Thu Hà - <span class="direct-chat-timestamp">Overlord chap 10</span> - <span class="direct-chat-timestamp">2:00 pm 23/3/2019</span></span>
+				</div>
+				<!-- /.direct-chat-info -->
+				<img class="direct-chat-img" src="{{ asset('image/admin/user3-128x128.jpg') }}" alt="Message User Image">
+				<!-- /.direct-chat-img -->
+				<div class="direct-chat-text">
+				Sed diam nonummy nibh euismod
+				</div>
+				<!-- /.direct-chat-text -->
+				<!-- tag comment -->
+				<div class="direct-chat-button">
+					<span class="fa fa-plus bg-blue" title="thêm vào danh sách kiểm tra" data-toggle="modal" data-target="#modal-comment-save"></span>
+					<span class="fa fa-flag bg-orange" title="phạt người dùng này" data-toggle="modal" data-target="#modal-comment-punish"></span>
+					<span class="fa fa-ban bg-red" title="cấm đăng nhập người dùng này" data-toggle="modal" data-target="#modal-comment-ban"></span>
+				</div>
+			</div>
+			<!-- /.direct-chat-msg -->
+			<div class="direct-chat-msg">
+				<div class="direct-chat-info clearfix">
+					<span class="direct-chat-name pull-left">Later Phát - <span class="direct-chat-timestamp">Overlord chap 10</span> - <span class="direct-chat-timestamp">2:00 pm 23/3/2019</span></span>
+				</div>
+				<!-- /.direct-chat-info -->
+				<img class="direct-chat-img" src="{{ asset('image/admin/user4-128x128.jpg') }}" alt="Message User Image">
+				<!-- /.direct-chat-img -->
+				<div class="direct-chat-text">
+				tincidunt ut laoreet dolore magna aliquam erat volutpat.
+				</div>
+				<!-- tag comment -->
+				<div class="direct-chat-button">
+					<span class="fa fa-plus bg-blue" title="thêm vào danh sách kiểm tra" data-toggle="modal" data-target="#modal-comment-save"></span>
+					<span class="fa fa-flag bg-orange" title="phạt người dùng này" data-toggle="modal" data-target="#modal-comment-punish"></span>
+					<span class="fa fa-ban bg-red" title="cấm đăng nhập người dùng này" data-toggle="modal" data-target="#modal-comment-ban"></span>
+				</div>
+				<!-- /.direct-chat-text -->
+			</div>
+			<!-- /.direct-chat-msg -->
+			<div class="direct-chat-msg">
+				<div class="direct-chat-info clearfix">
+					<span class="direct-chat-name pull-left">Hoàng Lê - <span class="direct-chat-timestamp">Overlord chap 10</span> - <span class="direct-chat-timestamp">2:00 pm 23/3/2019</span></span>
+				</div>
+				<!-- /.direct-chat-info -->
+				<img class="direct-chat-img" src="{{ asset('image/admin/user5-128x128.jpg') }}" alt="Message User Image">
+				<!-- /.direct-chat-img -->
+				<div class="direct-chat-text">
+				Ut wisi enim ad minim veniam.
+				</div>
+				<!-- /.direct-chat-text -->
+				<!-- tag comment -->
+				<div class="direct-chat-button">
+					<span class="fa fa-plus bg-blue" title="thêm vào danh sách kiểm tra" data-toggle="modal" data-target="#modal-comment-save"></span>
+					<span class="fa fa-flag bg-orange" title="phạt người dùng này" data-toggle="modal" data-target="#modal-comment-punish"></span>
+					<span class="fa fa-ban bg-red" title="cấm đăng nhập người dùng này" data-toggle="modal" data-target="#modal-comment-ban"></span>
+				</div>
+			</div>
+		</div>
+		<!--/.direct-chat-messages-->
+
+		<!-- /.direct-chat-pane -->
+	</div>
+</div>
