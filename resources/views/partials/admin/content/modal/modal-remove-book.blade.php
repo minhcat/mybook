36d2c40e3be4f07f7293e2b1c91fd3cb3ffeb668
Minh Cat <taminhcatcn12@gmail.com>
2019-05-19 12:07:32 +0700

    blade modal uploader

---
+++ resources/views/partials/admin/content/modal/modal-remove-book.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fade" id="modal-remove-book">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title">Xóa Truyện</h4>
+			</div>
+			<div class="modal-body">
+				<p>Bạn có chắc muốn xóa truyện này</p>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
+				<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
--- resources/views/partials/admin/content/uploader.blade.php
+++ resources/views/partials/admin/content/uploader.blade.php
@@ -218,71 +218,9 @@
 		</div>
 
 		<!-- Modal -->
-		<div class="modal fade" id="modal-keyword-new">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Thêm Từ Khóa</h4>
-					</div>
-					<div class="modal-body">
-						<div class="form-group">
-							<label for="">Từ khóa</label>
-							<input type="email" class="form-control" id="" placeholder="Thêm từ khóa">
-						</div>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
-						<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
-					</div>
-				</div>
-				<!-- /.modal-content -->
-			</div>
-			<!-- /.modal-dialog -->
-		</div>
+		@include('partials.admin.content.modal.modal-remove-book')
 
-		<div class="modal fade" id="modal-remove-book">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Xóa Truyện</h4>
-					</div>
-					<div class="modal-body">
-						<p>Bạn có chắc muốn xóa truyện này</p>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
-						<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
-					</div>
-				</div>
-				<!-- /.modal-content -->
-			</div>
-			<!-- /.modal-dialog -->
-		</div>
-
-		<div class="modal fade" id="modal-remove-chap">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Xóa Chap</h4>
-					</div>
-					<div class="modal-body">
-						<p>Bạn có chắc muốn xóa chap này</p>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
-						<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
-					</div>
-				</div>
-				<!-- /.modal-content -->
-			</div>
-			<!-- /.modal-dialog -->
-		</div>
+		@include('partials.admin.content.modal.modal-remove-chap')		
 	</section>
 	<!-- /.content -->
 </div>
