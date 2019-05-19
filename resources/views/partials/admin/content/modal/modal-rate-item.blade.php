fc4f9c9de78cf921ce2a07c05e46a039abcf8721
Minh Cat <taminhcatcn12@gmail.com>
2019-05-19 21:08:58 +0700

    blade modal statistic

---
+++ resources/views/partials/admin/content/modal/modal-rate-item.blade.php
@@ -0,0 +1,38 @@
+<div class="modal fade rate" id="modal-rate-item">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title">Chọn Loại Thống Kê</h4>
+			</div>
+			<div class="modal-body">
+				<div class="form-group">
+					<div class="radio">
+						<label>
+						<input type="radio" class="rate sum" name="radio-rate-item" checked="checked">
+						Lượt đánh giá
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+						<input type="radio" class="rate average" name="radio-rate-item">
+						Điểm trung bình
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+						<input type="radio" class="rate percent" name="radio-rate-item">
+						Phần trăm
+						</label>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer text-center">
+				<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
