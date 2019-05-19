184f9bd1923e798dae3939176d3d4e1f67b044c3
Minh Cat <taminhcatcn12@gmail.com>
2019-05-19 09:36:53 +0700

    blade comment uploader

---
+++ resources/views/partials/admin/content/comment/comment-new-book.blade.php
@@ -0,0 +1,77 @@
+<div class="box box-primary direct-chat direct-chat-primary collapse" id="box-book-comment" aria-expanded="false">
+	<!-- box header -->
+	<div class="box-header with-border">
+		<h3 class="box-title">Bình Luận Mới</h3>
+
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-remove="collapse"><i class="fa fa-times"></i></button>
+		</div>
+	</div>
+	<!-- /box header -->
+	<!-- box body -->
+	<div class="box-body">
+		<!-- Conversations are loaded here -->
+		<div class="direct-chat-messages">
+			<!-- Message. Default to the left -->
+			<div class="direct-chat-msg">
+				<div class="direct-chat-info clearfix">
+					<span class="direct-chat-name pull-left">Nguyễn Hoài Nam - <span class="direct-chat-timestamp">Overlord chap 10</span> - <span class="direct-chat-timestamp">2:00 pm 23/3/2019</span></span>
+				</div>
+				<!-- /.direct-chat-info -->
+				<img class="direct-chat-img" src="image/admin/user1-128x128.jpg" alt="Message User Image">
+				<!-- /.direct-chat-img -->
+				<div class="direct-chat-text">
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit
+				</div>
+
+				<!-- /.direct-chat-text -->
+			</div>
+			<!-- /.direct-chat-msg -->
+
+			<!-- Message to the right -->
+			<div class="direct-chat-msg">
+				<div class="direct-chat-info clearfix">
+					<span class="direct-chat-name pull-left">Thu Hà - <span class="direct-chat-timestamp">Overlord chap 10</span> - <span class="direct-chat-timestamp">2:00 pm 23/3/2019</span></span>
+				</div>
+				<!-- /.direct-chat-info -->
+				<img class="direct-chat-img" src="image/admin/user3-128x128.jpg" alt="Message User Image">
+				<!-- /.direct-chat-img -->
+				<div class="direct-chat-text">
+				Sed diam nonummy nibh euismod
+				</div>
+				<!-- /.direct-chat-text -->
+			</div>
+			<!-- /.direct-chat-msg -->
+			<div class="direct-chat-msg">
+				<div class="direct-chat-info clearfix">
+					<span class="direct-chat-name pull-left">Later Phát - <span class="direct-chat-timestamp">Overlord chap 10</span> - <span class="direct-chat-timestamp">2:00 pm 23/3/2019</span></span>
+				</div>
+				<!-- /.direct-chat-info -->
+				<img class="direct-chat-img" src="image/admin/user4-128x128.jpg" alt="Message User Image">
+				<!-- /.direct-chat-img -->
+				<div class="direct-chat-text">
+				tincidunt ut laoreet dolore magna aliquam erat volutpat.
+				</div>
+				<!-- /.direct-chat-text -->
+			</div>
+			<!-- /.direct-chat-msg -->
+			<div class="direct-chat-msg">
+				<div class="direct-chat-info clearfix">
+					<span class="direct-chat-name pull-left">Hoàng Lê - <span class="direct-chat-timestamp">Overlord chap 10</span> - <span class="direct-chat-timestamp">2:00 pm 23/3/2019</span></span>
+				</div>
+				<!-- /.direct-chat-info -->
+				<img class="direct-chat-img" src="image/admin/user5-128x128.jpg" alt="Message User Image">
+				<!-- /.direct-chat-img -->
+				<div class="direct-chat-text">
+				Ut wisi enim ad minim veniam.
+				</div>
+				<!-- /.direct-chat-text -->
+			</div>
+		</div>
+		<!--/.direct-chat-messages-->
+
+		<!-- /.direct-chat-pane -->
+	</div>
+</div>
---
+++ resources/views/partials/admin/content/list/list-chap.blade.php
@@ -0,0 +1,109 @@
+<div class="box box-primary collapse" id="box-chap-list" aria-expanded="false">
+	<div class="box-header">
+		<h3 class="box-title">Danh sách chap</h3>
+
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-remove="collapse"><i class="fa fa-times"></i></button>
+		</div>
+	</div>
+	<!-- /.box-header -->
+	<div class="box-body table-responsive no-padding">
+		<table class="table table-hover table-striped">
+			<tr>
+				<th>Stt</th>
+				<th>Số chap</th>
+				<th>Tựa chap</th>
+				<th>Ngày đăng</th>
+				<th>Số trang</th>
+				<th>Lượt xem</th>
+				<th>Sửa chap</th>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>Chap 1</td>
+				<td>Bacon ipsum dolor</td>
+				<td>11-7-2014</td>
+				<td>20</td>
+				<td>240</td>
+				<td><button class="btn btn-success">Sửa</button></td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>Chap 2</td>
+				<td>Bacon ipsum dolor</td>
+				<td>11-7-2014</td>
+				<td>20</td>
+				<td>240</td>
+				<td><button class="btn btn-success">Sửa</button></td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>Chap 2.5</td>
+				<td>Bacon ipsum dolor</td>
+				<td>11-7-2014</td>
+				<td>20</td>
+				<td>240</td>
+				<td><button class="btn btn-success">Sửa</button></td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>Chap 3</td>
+				<td>Bacon ipsum dolor</td>
+				<td>11-7-2014</td>
+				<td>20</td>
+				<td>240</td>
+				<td><button class="btn btn-success">Sửa</button></td>
+			</tr>
+			<tr>
+				<td>5</td>
+				<td>Chap 4</td>
+				<td>Bacon ipsum dolor</td>
+				<td>11-7-2014</td>
+				<td>20</td>
+				<td>240</td>
+				<td><button class="btn btn-success">Sửa</button></td>
+			</tr>
+			<tr>
+				<td>6</td>
+				<td>Chap 5</td>
+				<td>Bacon ipsum dolor</td>
+				<td>11-7-2014</td>
+				<td>20</td>
+				<td>240</td>
+				<td><button class="btn btn-success">Sửa</button></td>
+			</tr>
+			<tr>
+				<td>7</td>
+				<td>Chap 5.5</td>
+				<td>Bacon ipsum dolor</td>
+				<td>11-7-2014</td>
+				<td>20</td>
+				<td>240</td>
+				<td><button class="btn btn-success">Sửa</button></td>
+			</tr>
+			<tr>
+				<td>8</td>
+				<td>Chap 6</td>
+				<td>Bacon ipsum dolor</td>
+				<td>11-7-2014</td>
+				<td>20</td>
+				<td>240</td>
+				<td><button class="btn btn-success">Sửa</button></td>
+			</tr>
+		</table>
+	</div>
+	<!-- /.box-body -->
+	<div class="box-footer clearfix">
+		<button class="btn btn-primary box-link" data-target="#box-book-detail" data-unclose="#box-book-list-small">Chi tiết truyện</button>
+		<ul class="pagination pagination-sm no-margin pull-right">
+			<li><a href="#">&laquo;</a></li>
+			<li><a href="#">1</a></li>
+			<li><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li><a href="#">&raquo;</a></li>
+		</ul>
+	</div>
+	<!-- /.box footer -->
+</div>
