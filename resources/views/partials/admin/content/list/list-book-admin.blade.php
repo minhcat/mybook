e1c2cd9a013433a7249ce4a5fa67d94e8f56f22d
Minh Cat <taminhcatcn12@gmail.com>
2019-05-20 12:03:44 +0700

    blade list book chap admin

---
+++ resources/views/partials/admin/content/list/list-book-admin.blade.php
@@ -0,0 +1,118 @@
+<div class="box box-primary collapse in" id="box-book" aria-expanded="true">
+	<div class="box-header with-border">
+		<h3 class="box-title">Truyện</h3>
+
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-remove="collapse"><i class="fa fa-times"></i></button>
+		</div>
+	</div>
+	<div class="box-body">
+		<table id="table-book" class="table table-bordered table-striped">
+			<thead>
+				<tr>
+					<th>Id</th>
+					<th>Tên truyện</th>
+					<th>Ngày xuất bản</th>
+					<th>Ngày đăng</th>
+					<th>Người đăng</th>
+					<th class="fixed">Nội dung</th>
+					<th>Từ khóa</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>001</td>
+					<td>Attack On Titan</td>
+					<td>12/2/2008</td>
+					<td>29/4/2018</td>
+					<td>Võ Trinh</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...</td>
+					<td>Attack On Titan</td>
+				</tr>
+				<tr>
+					<td>002</td>
+					<td>Date A Live</td>
+					<td>12/2/2008</td>
+					<td>29/4/2018</td>
+					<td>Võ Trinh</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...</td>
+					<td>Date A Live</td>
+				</tr>
+				<tr>
+					<td>003</td>
+					<td>Dragon Ball</td>
+					<td>12/2/2008</td>
+					<td>29/4/2018</td>
+					<td>Võ Trinh</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...</td>
+					<td>Dragon Ball</td>
+				</tr>
+				<tr>
+					<td>004</td>
+					<td>Kakegurui</td>
+					<td>12/2/2008</td>
+					<td>29/4/2018</td>
+					<td>Võ Trinh</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...</td>
+					<td>Kakegurui</td>
+				</tr>
+				<tr>
+					<td>005</td>
+					<td>Konosuba</td>
+					<td>12/2/2008</td>
+					<td>29/4/2018</td>
+					<td>Võ Trinh</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...</td>
+					<td>Konosuba</td>
+				</tr>
+				<tr>
+					<td>006</td>
+					<td>Darling in the FranXX</td>
+					<td>12/2/2008</td>
+					<td>29/4/2018</td>
+					<td>Võ Trinh</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...</td>
+					<td>Darling in the FranXX</td>
+				</tr>
+				<tr>
+					<td>007</td>
+					<td>Overlord</td>
+					<td>12/2/2008</td>
+					<td>29/4/2018</td>
+					<td>Võ Trinh</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...</td>
+					<td>Overlord</td>
+				</tr>
+				<tr>
+					<td>008</td>
+					<td>Girly Air Force</td>
+					<td>12/2/2008</td>
+					<td>29/4/2018</td>
+					<td>Võ Trinh</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...</td>
+					<td>Girly Air Force</td>
+				</tr>
+				<tr>
+					<td>009</td>
+					<td>Golbin Slayer</td>
+					<td>12/2/2008</td>
+					<td>29/4/2018</td>
+					<td>Võ Trinh</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...</td>
+					<td>Golbin Slayer</td>
+				</tr>
+				<tr>
+					<td>010</td>
+					<td>Death Note</td>
+					<td>12/2/2008</td>
+					<td>29/4/2018</td>
+					<td>Võ Trinh</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...</td>
+					<td>Death Note</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
