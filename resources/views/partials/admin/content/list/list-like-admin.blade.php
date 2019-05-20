89ae40874ee0ddec0eb4e460cbda50b8b4f6ccbe
Minh Cat <taminhcatcn12@gmail.com>
2019-05-20 12:15:02 +0700

    blade contant list admin

---
+++ resources/views/partials/admin/content/list/list-like-admin.blade.php
@@ -0,0 +1,85 @@
+<div class="box box-primary collapse" id="box-like" aria-expanded="false">
+	<div class="box-header with-border">
+		<h3 class="box-title">Yêu thích</h3>
+
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-remove="collapse"><i class="fa fa-times"></i></button>
+		</div>
+	</div>
+	<div class="box-body">
+		<table id="table-like" class="table table-bordered table-hover">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Người yêu thích</th>
+					<th>Truyện yêu thích</th>
+					<th>Thời điểm yêu thích</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>001</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Kakegurui</td>
+					<td>10:00am 12/4/2018</td>
+				</tr>
+				<tr>
+					<td>002</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Kakegurui</td>
+					<td>9:00pm 13/5/2018</td>
+				</tr>
+				<tr>
+					<td>003</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Kakegurui</td>
+					<td>8:00am 15/5/2018</td>
+				</tr>
+				<tr>
+					<td>004</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Kakegurui</td>
+					<td>8:00am 8/3/2019</td>
+				</tr>
+				<tr>
+					<td>005</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Kakegurui</td>
+					<td>8:00am 8/3/2019</td>
+				</tr>
+				<tr>
+					<td>006</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Kakegurui</td>
+					<td>12:00am 9/5/2019</td>
+				</tr>
+				<tr>
+					<td>007</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Kakegurui</td>
+					<td>3:35pm 2/2/2010</td>
+				</tr>
+				<tr>
+					<td>008</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Kakegurui</td>
+					<td>8:00pm 3/3/2013</td>
+				</tr>
+				<tr>
+					<td>009</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Kakegurui</td>
+					<td>9:00am 4/4/2015</td>
+				</tr>
+				<tr>
+					<td>010</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Kakegurui</td>
+					<td>4:00am 6/6/2016</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
