93ce2af619f4aa557eca3c2430e007fae5d447e1
Minh Cat <taminhcatcn12@gmail.com>
2019-05-20 21:17:47 +0700

    blade timeline admin

---
+++ resources/views/partials/admin/content/list/list-staff-small.blade.php
@@ -0,0 +1,71 @@
+<div class="box box-primary collapse" id="box-staff-small" aria-expanded="false">
+	<div class="box-header">
+		<h3 class="box-title">Danh Sách Nhân Viên</h3>
+
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-remove="collapse"><i class="fa fa-times"></i></button>
+		</div>
+	</div>
+	<!-- /.box-header -->
+	<div class="box-body table-responsive no-padding">
+		<table class="table table-hover table-striped">
+			<tr>
+				<th>Stt</th>
+				<th>Tên nhân viên</th>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>Phạm Hoàng</td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>Phạm Hoàng</td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>Trần Hà</td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>Trần Hà</td>
+			</tr>
+			<tr>
+				<td>5</td>
+				<td>Lý Thường</td>
+			</tr>
+			<tr>
+				<td>6</td>
+				<td>Lý Thường</td>									
+			</tr>
+			<tr>
+				<td>7</td>
+				<td>Hoàng Lê</td>									
+			</tr>
+			<tr>
+				<td>8</td>
+				<td>Hoàng Lê</td>									
+			</tr>
+			<tr>
+				<td>9</td>
+				<td>Quang Võ</td>									
+			</tr>
+			<tr>
+				<td>10</td>
+				<td>Quang Võ</td>									
+			</tr>
+		</table>
+	</div>
+	<!-- /.box-body -->
+	<div class="box-footer clearfix">
+		<ul class="pagination pagination-sm no-margin pull-right">
+			<li><a href="#">&laquo;</a></li>
+			<li><a href="#">1</a></li>
+			<li><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li><a href="#">&raquo;</a></li>
+		</ul>
+	</div>
+	<!-- /.box footer -->
+</div>
---
+++ resources/views/partials/admin/content/timeline/timeline-all.blade.php
@@ -0,0 +1,89 @@
+<div class="box box-primary collapse" id="timeline-all" aria-expanded="false">
+	<div class="box-header with-border">
+		<h3 class="box-title">Dòng Thời Gian</h3>
+
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-remove="collapse"><i class="fa fa-times"></i></button>
+		</div>
+	</div>
+	<div class="box-body">
+		<ul class="timeline">
+			<li class="time-label">
+				<span class="bg-red">
+				10/2/2019
+				</span>
+			</li>
+			<li>
+				<i class="fa fa-upload bg-blue"></i>
+				<div class="timeline-item">
+					<span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+					<h3 class="timeline-header no-border"><a href="#">Nam Nguyễn</a> tải chap 10 truyện Overlord</h3>
+				</div>
+			</li>
+			<li>
+				<i class="fa fa-comment bg-green"></i>
+				<div class="timeline-item">
+					<span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+					<h3 class="timeline-header no-border"><a href="#">Võ Trinh</a> xử lý người dùng Mr.T</h3>
+				</div>
+			</li>
+			<li>
+				<i class="fa fa-envelope bg-teal"></i>
+				<div class="timeline-item">
+					<span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+					<h3 class="timeline-header"><a href="#">Lê Hữu Thọ</a> gữi cho bạn 1 mail</h3>
+					<div class="timeline-body">
+						Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem plugg dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle quora plaxo ideeli hulu weebly balihoo...
+					</div>
+					<div class="timeline-footer">
+						<a href="#" class="btn btn-primary btn-xs">chi tiết</a>
+						<a href="#" class="btn btn-danger btn-xs">xóa</a>
+					</div>
+				</div>
+			</li>
+			<li class="time-label">
+				<span class="bg-blue">
+				9/2/2019
+				</span>
+			</li>
+			<li>
+				<i class="fa fa-times bg-red"></i>
+				<div class="timeline-item">
+					<span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+					<h3 class="timeline-header no-border"><a href="#">Lê Hữu Thọ</a> xóa thể loại magic</h3>
+				</div>
+			</li>
+			<li>
+				<i class="fa fa-key bg-purple"></i>
+				<div class="timeline-item">
+					<span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+					<h3 class="timeline-header no-border"><a href="#">Phát Lê</a> cấp cho Nam Nguyễn quyền statistic</h3>
+				</div>
+			</li>
+			<li class="time-label">
+				<span class="bg-green">
+				8/2/2019
+				</span>
+			</li>
+			<li>
+				<i class="fa fa-sign-in bg-aqua"></i>
+				<div class="timeline-item">
+					<span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+					<h3 class="timeline-header no-border"><a href="#">Phạm Hoàng</a> đăng ký thành công vào trang admin</h3>
+				</div>
+			</li>
+			<li>
+				<i class="fa fa-upload bg-yellow"></i>
+				<div class="timeline-item">
+					<span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+					<h3 class="timeline-header no-border"><a href="#">Thắng Wrisse</a> tải lại chap 12 Attack on Titan</h3>
+				</div>
+			</li>
+			<li>
+				<i class="fa fa-clock-o bg-gray"></i>
+			</li>
+		</ul>
+	</div>
+</div>
