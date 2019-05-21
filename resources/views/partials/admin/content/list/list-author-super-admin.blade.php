91f080ad3dd68520b55e5f5c8e5a2f677f98c716
Minh Cat <taminhcatcn12@gmail.com>
2019-05-21 12:22:54 +0700

    blade list author, category, user, character, trans super admin

---
+++ resources/views/partials/admin/content/list/list-author-super-admin.blade.php
@@ -0,0 +1,195 @@
+<div class="box box-primary collapse" id="box-author" aria-expanded="false">
+	<div class="box-header with-border">
+		<h3 class="box-title">Tác giả</h3>
+
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-remove="collapse"><i class="fa fa-times"></i></button>
+		</div>
+	</div>
+	<div class="box-body">
+		<table id="table-author" class="table table-bordered table-hover">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Tên tác giả</th>
+					<th>Giới tính</th>
+					<th>Loại tác giả</th>
+					<th>Facebook</th>
+					<th>Twitter</th>
+					<th>Website</th>
+					<th>Sở thích</th>
+					<th>Giới thiệu</th>
+					<th class="column-fixed-210">Tùy chọn</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>001</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-success box-link" data-target="#box-edit-author" data-small="database">sửa</button>
+						<button class="btn btn-warning" data-toggle="modal" data-target="#modal-remove-temporary-author">xóa tạm</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-author">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>002</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-success box-link" data-target="#box-edit-author" data-small="database">sửa</button>
+						<button class="btn btn-warning" data-toggle="modal" data-target="#modal-remove-temporary-author">xóa tạm</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-author">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>003</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-success box-link" data-target="#box-edit-author" data-small="database">sửa</button>
+						<button class="btn btn-warning" data-toggle="modal" data-target="#modal-remove-temporary-author">xóa tạm</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-author">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>004</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-success box-link" data-target="#box-edit-author" data-small="database">sửa</button>
+						<button class="btn btn-warning" data-toggle="modal" data-target="#modal-remove-temporary-author">xóa tạm</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-author">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>005</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-success box-link" data-target="#box-edit-author" data-small="database">sửa</button>
+						<button class="btn btn-warning" data-toggle="modal" data-target="#modal-remove-temporary-author">xóa tạm</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-author">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>006</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-success">sửa</button>
+						<button class="btn btn-warning">xóa tạm</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>007</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-success">sửa</button>
+						<button class="btn btn-warning">xóa tạm</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>008</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-success">sửa</button>
+						<button class="btn btn-warning">xóa tạm</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>009</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-success">sửa</button>
+						<button class="btn btn-warning">xóa tạm</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>010</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-success">sửa</button>
+						<button class="btn btn-warning">xóa tạm</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="box-footer">
+		<button class="btn btn-primary box-link" data-target="#box-new-author" data-small="database">Thêm tác giả</button>
+		<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-all-author">Xóa toàn bộ</button>
+	</div>
+</div>
---
+++ resources/views/partials/admin/content/list/list-category-super-admin.blade.php
@@ -0,0 +1,139 @@
+<div class="box box-primary collapse" id="box-category" aria-expanded="false">
+	<div class="box-header with-border">
+		<h3 class="box-title">Thể loại</h3>
+
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-remove="collapse"><i class="fa fa-times"></i></button>
+		</div>
+	</div>
+	<div class="box-body">
+		<table id="table-category" class="table table-bordered table-hover">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th class="column-fixed-80">Tên thể loại</th>
+					<th class="column-fixed-80">Tên khác</th>
+					<th>Mô tả</th>
+					<th class="column-fixed-210">Tùy chọn</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>001</td>
+					<td>Action</td>
+					<td>Hành động</td>
+					<td>Thể loại này thường có nội dung về đánh nhau, bạo lực, hỗn loạn, với diễn biến nhanh</td>
+					<td>
+						<button class="btn btn-success box-link" data-target="#box-edit-category">sửa</button>
+						<button class="btn btn-warning" data-toggle="modal" data-target="#modal-remove-temporary-category">xóa tạm</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-category">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>002</td>
+					<td>Adult</td>
+					<td>Người lớn</td>
+					<td>Thể loại có đề cập đến vấn đề nhạy cảm, chỉ dành cho tuổi 17+</td>
+					<td>
+						<button class="btn btn-success box-link" data-target="#box-edit-category">sửa</button>
+						<button class="btn btn-warning" data-toggle="modal" data-target="#modal-remove-temporary-category">xóa tạm</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-category">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>003</td>
+					<td>Adventure</td>
+					<td>Phiêu lưu</td>
+					<td>Thể loại phiêu lưu, mạo hiểm, thường là hành trình của các nhân vật</td>
+					<td>
+						<button class="btn btn-success box-link" data-target="#box-edit-category">sửa</button>
+						<button class="btn btn-warning" data-toggle="modal" data-target="#modal-remove-temporary-category">xóa tạm</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-category">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>004</td>
+					<td>Comedy</td>
+					<td>Hài hước</td>
+					<td>Thể loại có nội dung trong sáng và cảm động, thường có các tình tiết gây cười, các xung đột nhẹ nhàng</td>
+					<td>
+						<button class="btn btn-success box-link" data-target="#box-edit-category">sửa</button>
+						<button class="btn btn-warning" data-toggle="modal" data-target="#modal-remove-temporary-category">xóa tạm</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-category">xóa vĩnh viễn</button>
+				</tr>
+				<tr>
+					<td>005</td>
+					<td>Cooking</td>
+					<td>Nấu ăn</td>
+					<td>Thể loại có nội dung về nấu ăn, ẩm thực</td>
+					<td>
+						<button class="btn btn-success box-link" data-target="#box-edit-category">sửa</button>
+						<button class="btn btn-warning" data-toggle="modal" data-target="#modal-remove-temporary-category">xóa tạm</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-category">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>006</td>
+					<td>Demon</td>
+					<td>Quỷ quái</td>
+					<td>Thể loại có yêu tố ma quái, quỷ dữ, quái vật,...</td>
+					<td>
+						<button class="btn btn-success">sửa</button>
+						<button class="btn btn-warning">xóa tạm</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>007</td>
+					<td>Doujinshi</td>
+					<td>Phóng tác</td>
+					<td>Thể loại truyện phóng tác do fan hay có thể cả những Mangaka khác với tác giả truyện gốc. Tác giả vẽ Doujinshi thường dựa trên những nhân vật gốc để viết ra những câu chuyện theo sở thích của mình</td>
+					<td>
+						<button class="btn btn-success">sửa</button>
+						<button class="btn btn-warning">xóa tạm</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>008</td>
+					<td>Ecchi</td>
+					<td>Gợi cảm</td>
+					<td>Thường có những tình huống nhạy cảm nhằm lôi cuốn người xem</td>
+					<td>
+						<button class="btn btn-success">sửa</button>
+						<button class="btn btn-warning">xóa tạm</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>009</td>
+					<td>Fantasy</td>
+					<td>Phép thuật</td>
+					<td>Thể loại xuất phát từ trí tưởng tượng phong phú, từ pháp thuật đến thế giới trong mơ thậm chí là những câu chuyện thần tiên</td>
+					<td>
+						<button class="btn btn-success">sửa</button>
+						<button class="btn btn-warning">xóa tạm</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>010</td>
+					<td>Mystery</td>
+					<td>Huyền bí</td>
+					<td>Thể loại thường xuất hiện những điều bí ấn không thể lí giải được và sau đó là những nỗ lực của nhân vật chính nhằm tìm ra câu trả lời thỏa đáng</td>
+					<td>
+						<button class="btn btn-success">sửa</button>
+						<button class="btn btn-warning">xóa tạm</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="box-footer">
+		<button class="btn btn-primary box-link" data-target="#box-new-category">Thêm thể loại</button>
+		<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-all-category">Xóa toàn bộ</button>
+	</div>
+</div>
