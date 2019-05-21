9700aed49ea626ebbc2877fd24dd05431698f7ab
Minh Cat <taminhcatcn12@gmail.com>
2019-05-21 21:16:05 +0700

    blade list temporary char, user, author, category, trans super admin

---
+++ resources/views/partials/admin/content/list/list-author-temporary.blade.php
@@ -0,0 +1,185 @@
+<div class="box box-primary collapse" id="box-author-temporary" aria-expanded="false">
+	<div class="box-header with-border">
+		<h3 class="box-title">Danh Sách Tác Giả Tạm Xóa</h3>
+
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-remove="collapse"><i class="fa fa-times"></i></button>
+		</div>
+	</div>
+	<div class="box-body">
+		<table id="table-author-temporary" class="table table-bordered table-hover">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Tên tác giả</th>
+					<th>Giới tính</th>
+					<th>Loại tác giả</th>
+					<th>Facebook</th>
+					<th>Twitter</th>
+					<th>Website</th>
+					<th>Sở thích</th>
+					<th>Giới thiệu</th>
+					<th class="column-fixed-170">Tùy chọn</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>001</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-author">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>002</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-author">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>003</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-author">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>004</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-author">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>005</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-author">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>006</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>007</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>008</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>009</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>010</td>
+					<td>Maruyama Kugane</td>
+					<td>Nam</td>
+					<td>Viết kịch bản</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Đang cập nhật</td>
+					<td>Thể thao</td>
+					<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="box-footer">
+		<button class="btn btn-primary">Khôi phục toàn bộ</button>
+		<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-all-author">Xóa toàn bộ</button>
+	</div>
+</div>
---
+++ resources/views/partials/admin/content/list/list-user-temporary.blade.php
@@ -0,0 +1,196 @@
+<div class="box box-primary collapse" id="box-user-temporary" aria-expanded="false">
+	<div class="box-header with-border">
+		<h3 class="box-title">Danh sách người dùng tạm xóa</h3>
+
+		<div class="box-tools pull-right">
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-remove="collapse"><i class="fa fa-times"></i></button>
+		</div>
+	</div>
+	<div class="box-body">
+		<table id="table-user-temporary" class="table table-bordered table-hover">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Tên người dùng</th>
+					<th>Biệt hiệu</th>
+					<th>Giới tính</th>
+					<th>Ngày sinh</th>
+					<th>Tính cách</th>
+					<th>Thể loại yêu thích</th>
+					<th>Facebook</th>
+					<th>Twitter</th>
+					<th>Câu logan</th>
+					<th class="column-fixed-170">Tùy chọn</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>001</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Otaku</td>
+					<td>Nam</td>
+					<td>12/3/2019</td>
+					<td>vui vẻ, hòa đồng</td>
+					<td>Action, Fantasy, Magic</td>
+					<td>Nam</td>
+					<td>Nam</td>
+					<td>Lorem ipsum</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-user">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>002</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Otaku</td>
+					<td>Nam</td>
+					<td>12/3/2019</td>
+					<td>vui vẻ, hòa đồng</td>
+					<td>Action, Fantasy, Magic</td>
+					<td>Nam</td>
+					<td>Nam</td>
+					<td>Lorem ipsum</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-user">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>003</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Otaku</td>
+					<td>Nam</td>
+					<td>12/3/2019</td>
+					<td>vui vẻ, hòa đồng</td>
+					<td>Action, Fantasy, Magic</td>
+					<td>Nam</td>
+					<td>Nam</td>
+					<td>Lorem ipsum</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-user">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>004</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Otaku</td>
+					<td>Nam</td>
+					<td>12/3/2019</td>
+					<td>vui vẻ, hòa đồng</td>
+					<td>Action, Fantasy, Magic</td>
+					<td>Nam</td>
+					<td>Nam</td>
+					<td>Lorem ipsum</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-user">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>005</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Otaku</td>
+					<td>Nam</td>
+					<td>12/3/2019</td>
+					<td>vui vẻ, hòa đồng</td>
+					<td>Action, Fantasy, Magic</td>
+					<td>Nam</td>
+					<td>Nam</td>
+					<td>Lorem ipsum</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-permanent-user">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>006</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Otaku</td>
+					<td>Nam</td>
+					<td>12/3/2019</td>
+					<td>vui vẻ, hòa đồng</td>
+					<td>Action, Fantasy, Magic</td>
+					<td>Nam</td>
+					<td>Nam</td>
+					<td>Lorem ipsum</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>007</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Otaku</td>
+					<td>Nam</td>
+					<td>12/3/2019</td>
+					<td>vui vẻ, hòa đồng</td>
+					<td>Action, Fantasy, Magic</td>
+					<td>Nam</td>
+					<td>Nam</td>
+					<td>Lorem ipsum</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>008</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Otaku</td>
+					<td>Nam</td>
+					<td>12/3/2019</td>
+					<td>vui vẻ, hòa đồng</td>
+					<td>Action, Fantasy, Magic</td>
+					<td>Nam</td>
+					<td>Nam</td>
+					<td>Lorem ipsum</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>009</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Otaku</td>
+					<td>Nam</td>
+					<td>12/3/2019</td>
+					<td>vui vẻ, hòa đồng</td>
+					<td>Action, Fantasy, Magic</td>
+					<td>Nam</td>
+					<td>Nam</td>
+					<td>Lorem ipsum</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+				<tr>
+					<td>010</td>
+					<td>Nguyễn Văn Nam</td>
+					<td>Otaku</td>
+					<td>Nam</td>
+					<td>12/3/2019</td>
+					<td>vui vẻ, hòa đồng</td>
+					<td>Action, Fantasy, Magic</td>
+					<td>Nam</td>
+					<td>Nam</td>
+					<td>Lorem ipsum</td>
+					<td>
+						<button class="btn btn-primary">khôi phục</button>
+						<button class="btn btn-danger">xóa vĩnh viễn</button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="box-footer">
+		<button class="btn btn-primary">Khôi phục toàn bộ</button>
+		<button class="btn btn-danger" data-toggle="modal" data-target="#modal-remove-all-user">Xóa toàn bộ</button>
+	</div>
+</div>
