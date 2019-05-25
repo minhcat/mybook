e8857fadcbf6083681489c90bf296a3adfdac3c4
Minh Cat <taminhcatcn12@gmail.com>
2019-05-25 20:49:58 +0700

    add books table

--- database/migrations/2019_05_25_132458_create_books_table.php
+++ database/migrations/2019_05_25_132458_create_books_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateBooksTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,14 +12,9 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
-		{
+		Schema::create('books', function (Blueprint $table) {
 			$table->increments('id');
 			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->rememberToken();
-			$table->timestamps();
 		});
 	}
 
@@ -30,7 +25,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::dropIfExists('books');
 	}
 
 }
