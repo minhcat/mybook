e45ad09ac4ba43101edf73aff25e6643f4d972ef
Minh Cat <taminhcatcn12@gmail.com>
2019-05-29 21:13:47 +0700

    add database topic rate home page

--- resources/views/partials/home/topic_rate.blade.php
+++ resources/views/partials/home/topic_rate.blade.php
@@ -3,11 +3,12 @@
 	<div class="clearfix"></div>
 	<div class="topic-content">
 		<div class="row">
+			@foreach ($books_rate as $book)
 			<div class="book col-lg-3 col-md-3 col-sm-6 col-xs-6 col-6">
 				<div class="book-content">
-					<img src="image/seiken-tsukai-no-kinshuu-eishou.jpg">
+					<img src="image/books/{{ $book->image }}.jpg">
 					<div class="book-info">
-						<div class="name">Seiken Tsukai no Kinshuu Eishou</div>
+						<div class="name">{{ $book->name }}</div>
 						<div class="group row">
 							<div class="star col-md-7 col-sm-6 col-xs-6">
 								<span class="fa fa-star"></span>
@@ -15,15 +16,15 @@
 								<span class="fa fa-star"></span>
 								<span class="fa fa-star-half-o"></span>
 								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
+								<span>{{ $book->rate_point }}</span>
 							</div>
 							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
+								<span>{{ $book->rate }} đánh giá</span>
 							</div>
 						</div>
 					</div>
 					<div class="book-hover bg-red">
-						<div class="name">Seiken Tsukai no Kinshuu...</div>
+						<div class="name">{{ $book->name }}</div>
 						<div class="type">phiêu lưu, giả tưởng, hài hước</div>
 						<div class="chap">25 tập</div>
 						<a href="detail-book.html" class="btn play">Xem Ngay</a>
@@ -34,295 +35,16 @@
 								<span class="fa fa-star"></span>
 								<span class="fa fa-star-half-o"></span>
 								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
+								<span>{{ $book->rate_point }}</span>
 							</div>
 							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
+								<span>{{ $book->rate }} đánh giá</span>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="book col-lg-3 col-md-3 col-sm-6 col-xs-6 col-6">
-				<div class="book-content">
-					<img src="image/seiken-tsukai-no-kinshuu-eishou.jpg">
-					<div class="book-info">
-						<div class="name">Seiken Tsukai no Kinshuu Eishou</div>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-					<div class="book-hover bg-red">
-						<div class="name">Seiken Tsukai no Kinshuu...</div>
-						<div class="type">phiêu lưu, giả tưởng, hài hước</div>
-						<div class="chap">25 tập</div>
-						<a href="detail-book.html" class="btn play">Xem Ngay</a>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="book col-lg-3 col-md-3 col-sm-6 col-xs-6 col-6">
-				<div class="book-content">
-					<img src="image/seiken-tsukai-no-kinshuu-eishou.jpg">
-					<div class="book-info">
-						<div class="name">Seiken Tsukai no Kinshuu Eishou</div>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-					<div class="book-hover bg-red">
-						<div class="name">Seiken Tsukai no Kinshuu...</div>
-						<div class="type">phiêu lưu, giả tưởng, hài hước</div>
-						<div class="chap">25 tập</div>
-						<a href="detail-book.html" class="btn play">Xem Ngay</a>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="book col-lg-3 col-md-3 col-sm-6 col-xs-6 col-6">
-				<div class="book-content">
-					<img src="image/seiken-tsukai-no-kinshuu-eishou.jpg">
-					<div class="book-info">
-						<div class="name">Seiken Tsukai no Kinshuu Eishou</div>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-					<div class="book-hover bg-red">
-						<div class="name">Seiken Tsukai no Kinshuu...</div>
-						<div class="type">phiêu lưu, giả tưởng, hài hước</div>
-						<div class="chap">25 tập</div>
-						<a href="detail-book.html" class="btn play">Xem Ngay</a>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="book col-lg-3 col-md-3 col-sm-6 col-xs-6 col-6">
-				<div class="book-content">
-					<img src="image/shokugeki-no-souma.jpg">
-					<div class="book-info">
-						<div class="name">Shokugeki no Souma</div>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-					<div class="book-hover bg-red">
-						<div class="name">Shokugeki no Souma</div>
-						<div class="type">Ẩm thực, tình cảm, hài hước</div>
-						<div class="chap">35 tập</div>
-						<a href="detail-book.html" class="btn play">Xem Ngay</a>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="book col-lg-3 col-md-3 col-sm-6 col-xs-6 col-6">
-				<div class="book-content">
-					<img src="image/shokugeki-no-souma.jpg">
-					<div class="book-info">
-						<div class="name">Shokugeki no Souma</div>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-					<div class="book-hover bg-red">
-						<div class="name">Shokugeki no Souma</div>
-						<div class="type">Ẩm thực, tình cảm, hài hước</div>
-						<div class="chap">35 tập</div>
-						<a href="detail-book.html" class="btn play">Xem Ngay</a>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="book col-lg-3 col-md-3 col-sm-6 col-xs-6 col-6">
-				<div class="book-content">
-					<img src="image/shokugeki-no-souma.jpg">
-					<div class="book-info">
-						<div class="name">Shokugeki no Souma</div>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-					<div class="book-hover bg-red">
-						<div class="name">Shokugeki no Souma</div>
-						<div class="type">Ẩm thực, tình cảm, hài hước</div>
-						<div class="chap">35 tập</div>
-						<a href="detail-book.html" class="btn play">Xem Ngay</a>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="book col-lg-3 col-md-3 col-sm-6 col-xs-6 col-6">
-				<div class="book-content">
-					<img src="image/shokugeki-no-souma.jpg">
-					<div class="book-info">
-						<div class="name">Shokugeki no Souma</div>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-					<div class="book-hover bg-red">
-						<div class="name">Shokugeki no Souma</div>
-						<div class="type">Ẩm thực, tình cảm, hài hước</div>
-						<div class="chap">35 tập</div>
-						<a href="detail-book.html" class="btn play">Xem Ngay</a>
-						<div class="group row">
-							<div class="star col-md-7 col-sm-6 col-xs-6">
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star"></span>
-								<span class="fa fa-star-half-o"></span>
-								<span class="fa fa-star-o"></span>
-								<span>3.5</span>
-							</div>
-							<div class="rate col-md-5 col-sm-6 col-xs-6">
-								<span>100 đánh giá</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			@endforeach
 		</div>
 	</div>
 	<div class="topic-more">
