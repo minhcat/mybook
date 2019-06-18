3fb073fcd1b605390cf096540d14c834e680d2d4
Minh Cat <taminhcatcn12@gmail.com>
2019-06-18 12:50:10 +0700

    blade modal mod

--- resources/views/partials/admin/content/mod.blade.php
+++ resources/views/partials/admin/content/mod.blade.php
@@ -62,229 +62,23 @@
 			</div>
 		</div>
 		<!-- Modal -->
-		<div class="modal fade" id="modal-comment-ban">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Chặn Người Dùng</h4>
-					</div>
-					<div class="modal-body">
-						<p>Bạn có chắc là muốn chặn người dùng này</p>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
-						<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
-					</div>
-				</div>
-				<!-- /.modal-content -->
-			</div>
-			<!-- /.modal-dialog -->
-		</div>
+		@include('partials.admin.content.modal.modal-ban-user')
 
-		<div class="modal fade" id="modal-comment-save">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Lưu Bình Luận Này</h4>
-					</div>
-					<div class="modal-body">
-						<p>Bình luận đã được lưu vào hệ thống</p>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
-						<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
-					</div>
-				</div>
-				<!-- /.modal-content -->
-			</div>
-			<!-- /.modal-dialog -->
-		</div>
+		@include('partials.admin.content.modal.modal-save-comment')
 
-		<div class="modal fade" id="modal-comment-punish">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Phạt Người Dùng</h4>
-					</div>
-					<div class="modal-body">
-						<p>Chọn hình thức phạt</p>
-						<div class="form-group">
-							<div class="radio">
-								<label>
-									<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-									Xóa bình luận
-								</label>
-							</div>
-							<div class="radio">
-								<label>
-									<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-									Cấm bình luận trong 5 ngày
-								</label>
-							</div>
-							<div class="radio">
-								<label>
-									<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-									Cấm bình luận trong 20 ngày
-								</label>
-							</div>
-							<div class="radio">
-								<label>
-									<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-									Cấm bình luận vĩnh viễn
-								</label>
-							</div>
-							<div class="radio">
-								<label>
-									<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-									Cấm đăng nhập trong 40 ngày
-								</label>
-							</div>
-						</div>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
-						<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
-					</div>
-				</div>
-				<!-- /.modal-content -->
-			</div>
-			<!-- /.modal-dialog -->
-		</div>
+		@include('partials.admin.content.modal.modal-punish-user')
 
-		<div class="modal fade" id="modal-comment-remove">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Xóa Bình Luận Khỏi Danh Sách</h4>
-					</div>
-					<div class="modal-body">
-						<p>Bạn có chắc xóa bình luận khỏi danh sách lưu</p>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
-						<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
-					</div>
-				</div>
-				<!-- /.modal-content -->
-			</div>
-			<!-- /.modal-dialog -->
-		</div>
+		@include('partials.admin.content.modal.modal-remove-comment-save')
 
-		<div class="modal fade" id="modal-checkword-add">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Thêm Từ Khóa</h4>
-					</div>
-					<div class="modal-body">
-						<div class="form-group">
-							<label for="">Từ khóa</label>
-							<input type="email" class="form-control" id="" placeholder="Thêm từ khóa">
-						</div>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
-						<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
-					</div>
-				</div>
-				<!-- /.modal-content -->
-			</div>
-			<!-- /.modal-dialog -->
-		</div>
+		@include('partials.admin.content.modal.modal-add-checkword')
 
-		<div class="modal fade" id="modal-checkword-remove">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Xóa Từ Khóa</h4>
-					</div>
-					<div class="modal-body">
-						<p>Bạn có chắc muốn xóa từ khóa này</p>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
-						<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
-					</div>
-				</div>
-				<!-- /.modal-content -->
-			</div>
-			<!-- /.modal-dialog -->
-		</div>
+		@include('partials.admin.content.modal.modal-remove-checkword')
 
-		<div class="modal fade" id="modal-user-remove-punishlist">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Xóa Khỏi Danh Sách Phạt</h4>
-					</div>
-					<div class="modal-body">
-						<p>Bạn có chắc xóa người dùng này khỏi danh sách phạt</p>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
-						<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
-					</div>
-				</div>
-				<!-- /.modal-content -->
-			</div>
-			<!-- /.modal-dialog -->
-		</div>
+		@include('partials.admin.content.modal.modal-remove-user-punish')
 
-		<div class="modal fade" id="modal-user-remove-banlist">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Gỡ Chặn</h4>
-					</div>
-					<div class="modal-body">
-						<p>Bạn có chắc xóa người dùng này khỏi danh sách chặn</p>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
-						<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
-					</div>
-				</div>
-				<!-- /.modal-content -->
-			</div>
-			<!-- /.modal-dialog -->
-		</div>
+		@include('partials.admin.content.modal.modal-remove-user-ban')
 
-		<div class="modal fade" id="modal-book-remove-follow">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Xóa Theo Dõi</h4>
-					</div>
-					<div class="modal-body">
-						<p>Bạn có chắc xóa truyện này khỏi danh sách theo dõi</p>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
-						<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
-					</div>
-				</div>
-				<!-- /.modal-content -->
-			</div>
-			<!-- /.modal-dialog -->
-		</div>
+		@include('partials.admin.content.modal.modal-remove-book-follow-comment')
 		
 	</section>
 	<!-- /.content -->
---
+++ resources/views/partials/admin/content/modal/modal-punish-user.blade.php
@@ -0,0 +1,52 @@
+<div class="modal fade" id="modal-comment-punish">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title">Phạt Người Dùng</h4>
+			</div>
+			<div class="modal-body">
+				<p>Chọn hình thức phạt</p>
+				<div class="form-group">
+					<div class="radio">
+						<label>
+							<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+							Xóa bình luận
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+							Cấm bình luận trong 5 ngày
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+							Cấm bình luận trong 20 ngày
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+							Cấm bình luận vĩnh viễn
+						</label>
+					</div>
+					<div class="radio">
+						<label>
+							<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+							Cấm đăng nhập trong 40 ngày
+						</label>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Hủy</button>
+				<button type="button" class="btn btn-primary" data-dismiss="modal">Đồng ý</button>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
