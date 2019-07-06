d50900ccc45c16bf36fb8944ece4af73f8e35c27
Minh Cat <taminhcatcn12@gmail.com>
2019-07-06 22:33:37 +0700

    change get chap with book is not deleted

--- app/Http/Models/QModels/ChapsQModel.php
+++ app/Http/Models/QModels/ChapsQModel.php
@@ -16,6 +16,7 @@ class ChapsQModel extends Model
 	public static function get_chap_by_id($id) {
 		$result = DB::table('chaps as c')
 				->join('books as b', 'b.id', '=', 'c.id_book')
+				->where('b.deleted', 0)
 				->where('c.id',$id)
 				->select('c.*', 'b.name as book_name', 'b.image', 'b.view', 'b.like','b.follow','b.description')
 				->get();
@@ -29,10 +30,12 @@ class ChapsQModel extends Model
 	 * @return object|boolean : all properties from `categories` table
 	 */
 	public static function get_chaps_by_book_id_trans_id($book_id, $trans_id) {
-		$result = DB::table('chaps')
-				->where('id_book',$book_id)
-				->where('id_trans',$trans_id)
-				->orderBy('index','desc')
+		$result = DB::table('chaps as c')
+				->join('books as b', 'b.id', '=', 'c.id_book')
+				->where('b.deleted', 0)
+				->where('c.id_book',$book_id)
+				->where('c.id_trans',$trans_id)
+				->orderBy('c.index','desc')
 				->get();
 
 		return $result;
@@ -44,11 +47,13 @@ class ChapsQModel extends Model
 	 * @return object|boolean : all properties from `categories` table
 	 */
 	public static function get_chap_by_book_id_trans_id_chap_slug($book_id, $trans_id, $chap_slug) {
-		$result = DB::table('chaps')
-				->where('id_book',$book_id)
-				->where('id_trans',$trans_id)
-				->where('slug',$chap_slug)
-				->orderBy('index','desc')
+		$result = DB::table('chaps as c')
+				->join('books as b', 'b.id', '=', 'c.id_book')
+				->where('b.deleted', 0)
+				->where('c.id_book',$book_id)
+				->where('c.id_trans',$trans_id)
+				->where('c.slug',$chap_slug)
+				->orderBy('c.index','desc')
 				->get();
 
 		return $result[0];
@@ -60,10 +65,12 @@ class ChapsQModel extends Model
 	 * @return object|boolean : all properties from `categories` table
 	 */
 	public static function get_trans_id_by_book_id($book_id) {
-		$result = DB::table('chaps')
-				->where('id_book',$book_id)
-				->groupBy('id_trans')
-				->select('id_trans')
+		$result = DB::table('chaps as c')
+				->join('books as b', 'b.id', '=', 'c.id_book')
+				->where('b.deleted', 0)
+				->where('c.id_book',$book_id)
+				->groupBy('c.id_trans')
+				->select('c.id_trans')
 				->get();
 
 		return $result;
@@ -75,10 +82,12 @@ class ChapsQModel extends Model
 	 * @return object|boolean : all properties from `categories` table
 	 */
 	public static function get_book_id_by_trans_id($trans_id) {
-		$result = DB::table('chaps')
-				->where('id_trans',$trans_id)
-				->groupBy('id_book')
-				->select('id_book')
+		$result = DB::table('chaps as c')
+				->join('books as b', 'b.id', '=', 'c.id_book')
+				->where('b.deleted', 0)
+				->where('c.id_trans',$trans_id)
+				->groupBy('c.id_book')
+				->select('c.id_book')
 				->get();
 
 		return $result;
@@ -93,6 +102,7 @@ class ChapsQModel extends Model
 		$result = DB::table('chaps as c')
 				->join('trans as t', 'c.id_trans', '=', 't.id')
 				->join('books as b', 'c.id_book', '=', 'b.id')
+				->where('b.deleted', 0)
 				->where('id_book',$book_id)
 				->select('c.*','t.name as trans_name','t.slug as trans_slug', 'b.slug as book_slug')
 				->get();
@@ -106,7 +116,9 @@ class ChapsQModel extends Model
 	 * @return object|boolean : all properties from `categories` table
 	 */
 	public static function count_chaps_by_book_id($book_id) {
-		$result = DB::table('chaps')
+		$result = DB::table('chaps as c')
+				->join('books as b', 'b.id', '=', 'c.id_book')
+				->where('b.deleted', 0)
 				->where('id_book', $book_id)
 				->groupBy('id_trans')
 				->selectRaw('id_trans, COUNT(id_trans) as chap_count')
