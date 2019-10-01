f7434ab0d3605b0fb143e4673f406d89f9381318
Minh Cat <taminhcatcn12@gmail.com>
2019-10-01 21:37:26 +0700

    set css blade setting

--- resources/views/pages/login/setting.blade.php
+++ resources/views/pages/login/setting.blade.php
@@ -22,95 +22,116 @@
 		</div>
 		@endif
 		<form class="setting">
-			<div class="set-label"><h4>Thông Báo</h4></div>
-			<hr>
-			<div class="notification">
-				<table>
-					<tr>
-						<td>Nhận thông báo từ ban quản trị</td>
-						<td>
-							<div id="cover">
-								<input type="checkbox" id="checkbox">
-								<div id="bar"></div>
-								<div id="knob"></div>
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td>Nhận thông báo từ người dùng tương tác</td>
-						<td>
-							<div id="cover">
-								<input type="checkbox" id="checkbox">
-								<div id="bar"></div>
-								<div id="knob"></div>
-							</div>
-						</td>
-					</tr>
-					<tr>
-						<td>Nhận thông báo từ đối tượng đang theo dõi</td>
-						<td>
-							<div id="cover">
-								<input type="checkbox" id="checkbox">
-								<div id="bar"></div>
-								<div id="knob"></div>
-							</div>
-						</td>
-					</tr>
-				</table>
-			</div>
-			<div class="set-label"><h4>Quyền Riêng Tư</h4></div>
-			<hr>
-			<div class="private">
-				<div class="set-item">
-					<p>Ai có thể xem thông tin cá nhân của bạn</p>
-					<div class="dropdown">
-						<button class="btn btn-private dropdown-toggle" type="button" data-toggle="dropdown">tùy chọn <span class="caret"></span></button>
-						<ul class="dropdown-menu">
-							<li>Tất cả mọi người</li>
-							<li>Bạn của bạn</li>
-							<li>Bạn bè</li>
-							<li>Chỉ mình tôi</li>
-						</ul>
-					</div>
-				</div>
-				<div class="set-item">
-					<p>Ai có thể tag bạn trong bình luận</p>
-					<div class="dropdown">
-						<button class="btn btn-private dropdown-toggle" type="button" data-toggle="dropdown">tùy chọn <span class="caret"></span></button>
-						<ul class="dropdown-menu">
-							<li>Tất cả mọi người</li>
-							<li>Bạn của bạn</li>
-							<li>Bạn bè</li>
-							<li>Chỉ mình tôi</li>
-						</ul>
-					</div>
-				</div>
-				<div class="set-item">
-					<p>Ai có thể gữi lời kết bạn đến bạn</p>
-					<div class="dropdown">
-						<button class="btn btn-private dropdown-toggle" type="button" data-toggle="dropdown">tùy chọn <span class="caret"></span></button>
-						<ul class="dropdown-menu">
-							<li>Tất cả mọi người</li>
-							<li>Bạn của bạn</li>
-							<li>Bạn bè</li>
-							<li>Chỉ mình tôi</li>
-						</ul>
-					</div>
+			<div class="group">
+				<div class="set-label"><h4>Thông Báo</h4></div>
+				<hr>
+				<div class="notification">
+					<table>
+						<tr>
+							<td>Nhận thông báo từ ban quản trị</td>
+							<td>
+								<div id="cover">
+									<input type="checkbox" id="checkbox">
+									<div id="bar"></div>
+									<div id="knob"></div>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>Nhận thông báo từ người dùng tương tác</td>
+							<td>
+								<div id="cover">
+									<input type="checkbox" id="checkbox">
+									<div id="bar"></div>
+									<div id="knob"></div>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td>Nhận thông báo từ đối tượng đang theo dõi</td>
+							<td>
+								<div id="cover">
+									<input type="checkbox" id="checkbox">
+									<div id="bar"></div>
+									<div id="knob"></div>
+								</div>
+							</td>
+						</tr>
+					</table>
 				</div>
 			</div>
-			<div class="set-label"><h4>Chặn</h4></div>
-			<hr>
-			<div class="block">
-				<div class="user">Nguyễn Võ <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
-				<div class="user">Võ Thị Quyên <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
-				<div class="user">Trương Tuấn Long Nhân <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
-				<div class="user">Minh Lê <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
-				<div class="user">Nguyễn Võ <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
-				<div class="user">Võ Thị Quyên <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
-				<div class="user">Trương Tuấn Long Nhân <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
-				<div class="user">Minh Lê <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
-				<div class="user">Lose Man <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
-				<div class="user">Cyber Archfiend <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
+			<div class="group">
+				<div class="set-label"><h4>Quyền Riêng Tư</h4></div>
+				<hr>
+				<div class="private">
+					<table>
+						<tr>
+							<td class="header">Ai có thể xem thông tin cá nhân của bạn</td>
+							<td class="select">Tất cả mọi người</td>
+							<td>
+								<div class="dropdown">
+									<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">tùy chọn <span class="caret"></span></button>
+									<ul class="dropdown-menu">
+										<li>Tất cả mọi người</li>
+										<li>Bạn của bạn</li>
+										<li>Bạn bè</li>
+										<li>Chỉ mình tôi</li>
+									</ul>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td class="header">Ai có thể tag bạn trong bình luận</td>
+							<td class="select">Tất cả mọi người</td>
+							<td>
+								<div class="dropdown">
+									<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">tùy chọn <span class="caret"></span></button>
+									<ul class="dropdown-menu">
+										<li>Tất cả mọi người</li>
+										<li>Bạn của bạn</li>
+										<li>Bạn bè</li>
+										<li>Chỉ mình tôi</li>
+									</ul>
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td class="header">Ai có thể gữi lời kết bạn đến bạn</td>
+							<td class="select">Tất cả mọi người</td>
+							<td>
+								<div class="dropdown">
+									<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">tùy chọn <span class="caret"></span></button>
+									<ul class="dropdown-menu">
+										<li>Tất cả mọi người</li>
+										<li>Bạn của bạn</li>
+										<li>Bạn bè</li>
+										<li>Chỉ mình tôi</li>
+									</ul>
+								</div>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<div class="group">
+				<div class="set-label"><h4>Chặn</h4></div>
+				<hr>
+				<div class="block">
+					<div class="user">Nguyễn Võ <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
+					<div class="user">Võ Thị Quyên <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
+					<div class="user">Trương Tuấn Long Nhân <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
+					<div class="user">Minh Lê <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
+					<div class="user">Nguyễn Võ <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
+					<div class="user">Võ Thị Quyên <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
+					<div class="user">Trương Tuấn Long Nhân <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
+					<div class="user">Minh Lê <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
+					<div class="user">Lose Man <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
+					<div class="user">Cyber Archfiend <span class="cancel-block open-modal" data-modal="#modalblock">&times;</span></div>
+				</div>
+			</div>
+			<div class="line clearfix submit">
+				<div class="col-md-6"><button type="button" class="submit form-control">Thay Đổi</button></div>
+				<div class="col-md-6"><button type="reset" class="reset form-control">Nhập lại</button></div>
 			</div>
 		</form>
 	</div>
